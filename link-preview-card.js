f4d3f954bdde83066644db55254d580129a898f8
PlayGamesMakeGames <tjlakatos89@gmail.com>
2025-03-23 17:37:58 -0400

    Finished Demo

--- link-preview-card.js
+++ link-preview-card.js
@@ -29,6 +29,7 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
     this.jsonLink = "";
     this.jsonThemeColor = "";
     document.documentElement.style.setProperty('--ddd-primary-x', `var(--ddd-primary-${Math.floor(Math.random() * 26)})`);
+    document.documentElement.style.setProperty('--ddd-default-x', `var(--ddd-default-${Math.floor(Math.random() * 26)})`);
     this.loadingState = false;
     this.t = this.t || {};
     this.t = {
@@ -86,13 +87,20 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
       }
       /* card is invis when invalid link is pasted, not invis when valid link pasted */
       .card{
-        display: inline-block;
+        display: inline-flex;
         opacity: 1;
         pointer-events: all;
+        margin: var(--ddd-spacing-2);
+        border: var(--ddd-border-md);
+        border-radius: var(--ddd-radius-lg);
+        padding: var(--ddd-spacing-3);
+        max-width: var(--ddd-breakpoint-lg);
+        max-height: var(--ddd-breakpoint-lg);
+        overflow: auto;
       }
       .cardInvis{
         display: none;
-        /* opacity: 0; */
+        opacity: 0;
         pointer-events: none;
         height: 0px;
         width: 0px;
@@ -105,18 +113,37 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
       height: 120px;
       animation: spin 2s linear infinite;
     }
-    #cardImgid{
-      max-width: 512px;
-      max-height: 256px;
+    
+    @media(min-width: 513px){
+      #cardImgid{
+        max-width: 512px;
+        max-height: 256px;
+        padding: var(--ddd-spacing-3);
+      }
+    }
+    @media(max-width: 512px){
+      /* :host{
+        transform: scale(.5);
+      } */
+      #cardImgid{
+      max-width: 256px;
+      max-height: 128px;
+    }
     }
     .card.psucolor {
       color: var(--ddd-primary-2); //nittany navy if psu based domain
+      border-color: var(--ddd-primary-2);
+      border-color: var(--ddd-default-2);
     }
     .card.default {
       color: var(--ddd-primary-x); //rand color between 0 and 25, set in constructor
+      border-color: var(--ddd-primary-x);
+      border-color: var(--ddd-default-x);
     }
     .card.themecolor {
-      color: var(--theme-color);
+      color: var(--theme-color); //json theme color
+      border-color: var(--theme-color);
+      border-color: var(--theme-color);
     }
 
     @keyframes spin {
@@ -151,7 +178,7 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
       console.log(this.jsonImg);
       console.log(this.jsonLink);
       console.log(this.jsonThemeColor);
-      this.style.setProperty('--theme-color', this.jsonThemeColor || 'gray');
+      this.style.setProperty('--theme-color', this.jsonThemeColor || '--ddd-primary-x');
       return true;
     } catch (error) {
       console.error(error.message);
@@ -225,10 +252,10 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
       }
       else{
         console.log("invaliddddd");
-        // this.jsonTitle = "";
-        // this.jsonDesc = "";
-        // this.jsonImg = "";
-        // this.jsonLink = "";
+        this.jsonTitle = "";
+        this.jsonDesc = "";
+        this.jsonImg = "";
+        this.jsonLink = "";
         // this.jsonThemeColor = "";
         this.shadowRoot.querySelector(".card").classList.add("cardInvis");
         this.shadowRoot.querySelector(".card").classList.add("default");
@@ -255,10 +282,10 @@ export class LinkPreviewCard extends DDDSuper(I18NMixin(LitElement)) {
   <!-- <textarea class="textField" id="textFieldid" @paste="${this.updateLink}" style="display: ${this.loadingState ? 'none' : 'block'};">${this.textValue}</textarea> -->
   <!-- card that appears below link maybe set initially to disabled, enable when valid link? -->
   <div class="card cardInvis ${this.themeColor ? 'themecolor' : 'default'}" style="display: ${this.loadingState ? 'none' : 'block'};">
-    Title: ${this.jsonTitle} <br>
-    <img src=${this.jsonImg} id="cardImgid"> <br>
-    Description: ${this.jsonDesc} <br>
-    Link: <a href="${this.jsonLink}">${this.jsonLink}</a>
+    ${this.jsonTitle ? html`Title: ${this.jsonTitle} <br>` : html``}
+    ${this.jsonImg ? html`<img src=${this.jsonImg} id="cardImgid"> <br>` : html``}
+    ${this.jsonDesc ? html`Description: ${this.jsonDesc} <br>` : html``}
+    ${this.jsonLink ? html`Link: <a href="${this.jsonLink}">${this.jsonLink}</a>` : html``}
   </div>
 </div>`;
   }
